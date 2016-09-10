a888adccc0c912274a662fca08c0535718b4ee11
Vladimír Kriška <vlado@keboola.com>
2016-09-10 23:50:50 +0200

    Add tests for empty relations and wrong paths

---
+++ tests/Keboola/CsvMap/MapperEmptyRelationTest.php
@@ -0,0 +1,108 @@
+<?php
+
+use Keboola\CsvMap\Mapper;
+
+class MapperEmptyRelationTest extends PHPUnit_Framework_TestCase
+{
+    public function testParseEmptyRelation()
+    {
+        $mapping = [
+            'id' => [
+                'type' => 'column',
+                'mapping' => [
+                    'destination' => 'pk',
+                    'primaryKey' => true,
+                ]
+            ],
+            'timestamp' => 'timestamp',
+            'reactions' => [
+                'type' => 'table',
+                'destination' => 'reactions',
+                'tableMapping' => [
+                    'user.id' => 'id',
+                    'user.username' => 'username',
+                ]
+            ]
+        ];
+
+        // one row
+        $data = $this->getSampleDataSimple();
+
+        $parser = new Mapper($mapping);
+        $parser->parse($data);
+
+        $file1 = $parser->getCsvFiles()['root'];
+        $expected1 = <<<CSV
+"pk","timestamp"
+"1","1234567890"\n
+CSV;
+        $this->assertFileExists($file1->getPathname());
+        $this->assertEquals($expected1, file_get_contents($file1->getPathname()));
+
+
+        $file2 = $parser->getCsvFiles()['reactions'];
+
+        $expected2 = <<<CSV
+"id","username","root_pk"\n
+CSV;
+        $this->assertFileExists($file2->getPathname());
+        $this->assertEquals($expected2, file_get_contents($file2->getPathname()));
+
+        // multiple rows
+        $data = $this->getSampleDataMulti();
+
+        $parser = new Mapper($mapping);
+        $parser->parse($data);
+
+        $file1 = $parser->getCsvFiles()['root'];
+        $expected1 = <<<CSV
+"pk","timestamp"
+"1","1234567890"
+"2","9876543210"\n
+CSV;
+        $this->assertFileExists($file1->getPathname());
+        $this->assertEquals($expected1, file_get_contents($file1->getPathname()));
+
+
+        $file2 = $parser->getCsvFiles()['reactions'];
+
+        $expected2 = <<<CSV
+"id","username","root_pk"\n
+CSV;
+        $this->assertFileExists($file2->getPathname());
+        $this->assertEquals($expected2, file_get_contents($file2->getPathname()));
+    }
+
+    protected function getSampleDataSimple()
+    {
+        $json = <<<JSON
+[
+    {
+        "timestamp": 1234567890,
+        "id": 1,
+        "reactions": []
+    }
+]
+JSON;
+        return json_decode($json, true);
+    }
+
+    protected function getSampleDataMulti()
+    {
+        $json = <<<JSON
+[
+    {
+        "id": 1,
+        "timestamp": 1234567890,
+        "reactions": []
+    },
+    {
+        "id": 2,
+        "timestamp": 9876543210,
+        "reactions": []
+    }
+]
+JSON;
+        return json_decode($json, true);
+    }
+}
