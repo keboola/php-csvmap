e1060de05d5a4e320b3f3093cca350e3caa94db3
Vladimír Kriška <vlado@keboola.com>
2016-09-11 17:28:25 +0200

    Add additional test
    Test where only few items has empty relation

--- tests/Keboola/CsvMap/MapperEmptyRelationTest.php
+++ tests/Keboola/CsvMap/MapperEmptyRelationTest.php
@@ -73,6 +73,56 @@ CSV;
         $this->assertEquals($expected2, file_get_contents($file2->getPathname()));
     }
 
+    public function testParseEmptyRelationFew()
+    {
+        $mapping = [
+            'id' => [
+                'type' => 'column',
+                'mapping' => [
+                    'destination' => 'pk',
+                    'primaryKey' => true,
+                ]
+            ],
+            'timestamp' => 'timestamp',
+            'reactions' => [
+                'type' => 'table',
+                'destination' => 'reactions',
+                'tableMapping' => [
+                    'user.id' => 'id',
+                    'user.username' => 'username',
+                ]
+            ]
+        ];
+
+        // multiple rows
+        $data = $this->getSampleDataMultiFirstEmpty();
+
+        $parser = new Mapper($mapping);
+        $parser->parse($data);
+
+        $file1 = $parser->getCsvFiles()['root'];
+        $expected1 = <<<CSV
+"pk","timestamp"
+"1","1234567891"
+"2","1234567892"
+"3","1234567893"
+"4","1234567894"\n
+CSV;
+        $this->assertFileExists($file1->getPathname());
+        $this->assertEquals($expected1, file_get_contents($file1->getPathname()));
+
+
+        $file2 = $parser->getCsvFiles()['reactions'];
+
+        $expected2 = <<<CSV
+"id","username","root_pk"
+"456","jose","2"
+"789","mary","4"\n
+CSV;
+        $this->assertFileExists($file2->getPathname());
+        $this->assertEquals($expected2, file_get_contents($file2->getPathname()));
+    }
+
     protected function getSampleDataSimple()
     {
         $json = <<<JSON
@@ -100,6 +150,50 @@ JSON;
         "id": 2,
         "timestamp": 9876543210,
         "reactions": []
+    }
+]
+JSON;
+        return json_decode($json, true);
+    }
+
+
+    protected function getSampleDataMultiFirstEmpty()
+    {
+        $json = <<<JSON
+[
+    {
+        "id": 1,
+        "timestamp": 1234567891,
+        "reactions": []
+    },
+    {
+        "id": 2,
+        "timestamp": 1234567892,
+        "reactions": [
+            {
+                "user": {
+                    "id": 456,
+                    "username": "jose"
+                }
+            }
+        ]
+    },
+    {
+        "id": 3,
+        "timestamp": 1234567893,
+        "reactions": []
+    },
+    {
+        "id": 4,
+        "timestamp": 1234567894,
+        "reactions": [
+            {
+                "user": {
+                    "id": 789,
+                    "username": "mary"
+                }
+            }
+        ]
     }
 ]
 JSON;
